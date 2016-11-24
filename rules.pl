4d21c6f771ff4c11d55fd662d8145af32b508fa3
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2016-11-24 20:46:09 +0100

    Find players for best game and given line

--- rules.pl
+++ rules.pl
@@ -1,18 +1,21 @@
 sumGoals(L, Sum) :- findall(W, scoredGoal(_, line(L), W), Ws), sumlist(Ws, Sum).
 
 minlist([H], H).
-minlist([H|T], M2):- minlist(T, M), M2 is min(H, M).
+minlist([H|T], M2) :- minlist(T, M), M2 is min(H, M).
 
 maxlist([H], H).
-maxlist([H|T], M2):- maxlist(T, M), M2 is max(H, M).
+maxlist([H|T], M2) :- maxlist(T, M), M2 is max(H, M).
 
-bestGame(Line, game(BestGame)):- 
-	findall(Game, scoredGoal(_, line(Line), Game), GameList), 
+line_setup(Line, player(X)) :- 
+	findall(Goal, scoredGoal(_, line(Line), Goal), GameList), 
 	maxlist(GameList, Goals), 
-	scoredGoal(game(BestGame), line(Line), Goals).
+	bestGame(Line, Goals, BestGame),
+	player_plays(player(X), game(BestGame), line(Line)).
 
-worstGame(Line, game(BestGame)):- 
-	findall(Game, scoredGoal(_, line(Line), Game), GameList),  
+bestGame(Line, Goals, Game) :- scoredGoal(game(Game), line(Line), Goals). 
+
+worstGame(Line, game(WorstGame)):- 
+	findall(Goal, scoredGoal(_, line(Line), Goal), GameList),  
 	minlist(GameList, Goals), 
-	scoredGoal(game(BestGame), line(Line), Goals).
+	scoredGoal(game(WorstGame), line(Line), Goals).
 
