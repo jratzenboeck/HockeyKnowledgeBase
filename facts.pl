6061947fe10b9d7051408255b2bdb0020e88bc8c
Berthold Roiser <berthold.roiser@gmail.com>
2016-11-26 12:36:45 +0100

    line_setup_goalie

--- facts.pl
+++ facts.pl
@@ -1,31 +1,35 @@
-player('michael ouzas', goalie).
-player('florian janny', goalie).
-player('dominic divis', goalie).
-player('jonathan daversa', defender).
-player('brett palin', defender).
-player('marc andre dorion', defender).
-player('niklas mayrhauser', defender).
-player('gerd kragl', defender).
-player('erik kirchschlaeger', defender).
-player('bernhard fechtig', defender).
-player('robert lukas', defender).
-player('mario altmann', defender).
-player('sebastien piche', defender).
-player('philipp lukas', forward).
-player('daniel oberkofler', forward).
-player('rick schofield', forward).
-player('stefan gaffal', forward).
-player('laurens ober', forward).
-player('brett mclean', forward).
-player('joel broda', forward).
-player('fabio hofer', forward).
-player('curtis loik', forward).
-player('stefan freunschlag', forward).
-player('kevin moderer', forward).
-player('patrick spannring', forward).
-player('dan dasilva', forward).
-player('mate gaspar', forward).
-player('kevin moderer', forward).
+position(goalie).
+position(defender).
+position(forward).
+
+player('michael ouzas'). %goalie
+player('florian janny'). %goalie
+player('dominic divis'). %goalie
+player('jonathan daversa'). %defender
+player('brett palin'). %defender
+player('marc andre dorion'). %defender
+player('niklas mayrhauser'). %defender
+player('gerd kragl'). %defender
+player('erik kirchschlaeger'). %defender
+player('bernhard fechtig'). %defender
+player('robert lukas'). %defender
+player('mario altmann'). %defender
+player('sebastien piche'). %defender
+player('philipp lukas'). %forward
+player('daniel oberkofler'). %forward
+player('rick schofield'). %forward
+player('stefan gaffal'). %forward
+player('laurens ober'). %forward
+player('brett mclean'). %forward
+player('joel broda'). %forward
+player('fabio hofer'). %forward
+player('curtis loik'). %forward
+player('stefan freunschlag'). %forward
+player('kevin moderer'). %forward
+player('patrick spannring'). %forward
+player('dan dasilva'). %forward
+player('mate gaspar'). %forward
+player('kevin moderer'). %forward
 
 line(l1).
 line(l2).
@@ -36,30 +40,26 @@ game(g1).
 game(g2).
 
 scoredGoal(game(g1), line(l1), 1, 2).
-scoredGoal(game(g1), line(l2), 1, 0).
+scoredGoal(game(g1), line(l2), 1, 1).
 scoredGoal(game(g1), line(l3), 1, 2).
 scoredGoal(game(g1), line(l4), 1, 0).
 scoredGoal(game(g2), line(l1), 4, 4).
 
-receivedGoal(game(g1), line(l1), 2).
-receivedGoal(game(g2), line(l1), 4).
-receivedGoal(game(g1), line(l2), 0).
-receivedGoal(game(g1), line(l3), 2).
-receivedGoal(game(g1), line(l4), 0).
+%game1
+player_plays(player('michael ouzas'), position(goalie), game(g1), _).
+player_plays(player('philipp lukas'), position(forward), game(g1), line(l1)).
+player_plays(player('fabio hofer'), position(forward), game(g1), line(l1)).
+player_plays(player('dan dasilva'), position(forward), game(g1), line(l1)).
+player_plays(player('robert lukas'), position(defender), game(g1), line(l1)).
+player_plays(player('marc andre dorion'), position(defender), game(g1), line(l1)).
 
-player_plays(player('philipp lukas', forward), game(g1), line(l1)).
-player_plays(player('fabio hofer', forward), game(g1), line(l1)).
-player_plays(player('dan dasilva', forward), game(g1), line(l1)).
-player_plays(player('robert lukas', defender), game(g1), line(l1)).
-player_plays(player('marc andre dorion', defender), game(g1), line(l1)).
-
-player_plays(player('curtis loik', forward), game(g2), line(l1)).
-player_plays(player('kevin moderer', forward), game(g2), line(l1)).
-player_plays(player('rob hisey', forward), game(g2), line(l1)).
-player_plays(player('robert lukas', defender), game(g2), line(l1)).
-player_plays(player('mario altmann', defender), game(g2), line(l1)).
-
-
+%game2
+player_plays(player('florian janny'), position(goalie), game(g2), _).
+player_plays(player('curtis loik'), position(forward), game(g2), line(l1)).
+player_plays(player('kevin moderer'), position(forward), game(g2), line(l1)).
+player_plays(player('rob hisey'), position(forward), game(g2), line(l1)).
+player_plays(player('robert lukas'), position(defender), game(g2), line(l1)).
+player_plays(player('mario altmann'), position(defender), game(g2), line(l1)).
 
 
 
--- rules.pl
+++ rules.pl
@@ -1,41 +1,62 @@
 min_list([H], H).
-min_list([H|T], M2) :- min_list(T, M), M2 is min(H, M).
+min_list([H|T], M2) :- 
+	min_list(T, M), M2 is min(H, M).
 
 max_list([H], H).
-max_list([H|T], M2) :- max_list(T, M), M2 is max(H, M).
-	
-line_setup_offense(Line, Player) :- 
-	findall(ScoredGoals, scoredGoal(_, line(Line), ScoredGoals, _), ScoredGoalsForGames), 
-	max_list(ScoredGoalsForGames, MaxScoredGoals),
-	best_offense_game(Line, MaxScoredGoals, BestOffenseGame),
-	player_plays(player(Player, forward), BestOffenseGame, line(Line)).
+max_list([H|T], M2) :- 
+	max_list(T, M), M2 is max(H, M).
 
-line_setup_defense(Line, Player) :-
-	findall(ReceivedGoals, scoredGoal(_, line(Line), _, ReceivedGoals), ReceivedGoalsForGames), 
-	min_list(ReceivedGoalsForGames, MinReceivedGoals),
-	best_defense_game(Line, MinReceivedGoals, BestDefenseGame),
-	player_plays(player(Player, defender), BestDefenseGame, line(Line)).
+indexOf([Element|_], Element, 1). 					% We found the element
+indexOf([_|Tail], Element, Index) :-
+  	indexOf(Tail, Element, Index1), 				% Check in the tail of the list
+  	Index is Index1 + 1.  						% and increment the resulting index
 
-best_offense_game(Line, MaxScoredGoals, BestOffenseGame) :- 
-	scoredGoal(BestOffenseGame, line(Line), MaxScoredGoals, _).
-
-best_defense_game(Line, MinReceivedGoals, BestDefenseGame):- 
-	scoredGoal(BestDefenseGame, line(Line), _, MinReceivedGoals).
-
-line_setup_for_best_plus_minus(Line, Player) :-
-	findall(PlusMinus, plus_minus(_, Line, PlusMinus), PlusMinusForGames),
-	max_list(PlusMinusForGames, MaxPlusMinus),
-	indexOf(PlusMinusForGames, MaxPlusMinus, Index),
-	atom_concat(g, Index, Game),
-	player_plays(player(Player, _), game(Game), line(Line)).
-	
-indexOf([Element|_], Element, 1). % We found the element
-indexOf([_|Tail], Element, Index):-
-  indexOf(Tail, Element, Index1), % Check in the tail of the list
-  Index is Index1 + 1.  % and increment the resulting index
-
-plus_minus(Game, Line, Score) :-
+plus_minus_goals(Game, Line, Score) :-
 	scoredGoal(game(Game), line(Line), GoalsScored, GoalsReceived),
 	Score is GoalsScored - GoalsReceived.
 
-sumGoals(L, Sum) :- findall(W, scoredGoal(_, line(L), W), Ws), sumlist(Ws, Sum).
+received_goals(Game, Score) :- 
+	findall(ReceivedGoal, scoredGoal(game(Game), _, _, ReceivedGoal), ReceivedGoals), 
+	sumlist(ReceivedGoals, Score).
+
+best_offense_game(Line, BestOffenseGame) :- 
+	findall(ScoredGoals, scoredGoal(_, line(Line), ScoredGoals, _), ScoredGoalsForGames), 
+	max_list(ScoredGoalsForGames, MaxScoredGoals),
+	scoredGoal(BestOffenseGame, line(Line), MaxScoredGoals, _).
+
+best_defense_game(Line, BestDefenseGame) :- 
+	findall(ReceivedGoals, scoredGoal(_, line(Line), _, ReceivedGoals), ReceivedGoalsForGames), 
+	min_list(ReceivedGoalsForGames, MinReceivedGoals),
+	scoredGoal(BestDefenseGame, line(Line), _, MinReceivedGoals).
+
+best_goalie_game(game(BestDefenseGame)) :- 
+	findall(Game, scoredGoal(game(Game), _, _, _), GamesAsList), 
+	list_to_set(GamesAsList, GamesAsSet),  
+	maplist(received_goals, GamesAsSet, ReceivedGoalsForGames),
+	min_list(ReceivedGoalsForGames, MinReceivedGoals),
+	indexOf(ReceivedGoalsForGames, MinReceivedGoals, Index),
+	atom_concat(g, Index, BestDefenseGame).
+
+best_plus_minus_game(Line, game(BestPlusMinusGame)) :-
+	findall(PlusMinus, plus_minus_goals(_, Line, PlusMinus), PlusMinusForGames),
+	max_list(PlusMinusForGames, MaxPlusMinus),
+	indexOf(PlusMinusForGames, MaxPlusMinus, Index),
+	atom_concat(g, Index, BestPlusMinusGame).
+	
+line_setup_offense(Line, Player) :- 
+	best_offense_game(Line, BestOffenseGame),
+	player_plays(player(Player), position(forward), BestOffenseGame, line(Line)).
+
+line_setup_defense(Line, Player) :-
+	best_defense_game(Line, BestDefenseGame),
+	player_plays(player(Player), position(defender), BestDefenseGame, line(Line)).
+
+line_setup_goalie(Line, Player) :-
+	best_goalie_game(BestGoalieGame),
+	player_plays(player(Player), position(goalie), BestGoalieGame, line(Line)).
+
+line_setup_plus_minus(Line, Player) :-
+	best_plus_minus_game(Line, BestPlusMinusGame),
+	player_plays(player(Player), position(_), BestPlusMinusGame, line(Line)).
+
+
